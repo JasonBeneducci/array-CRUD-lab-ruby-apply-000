5dffa2cdbde56804c0537dc76c57a104107c214c
Jason Beneducci <jasonbeneducci@yahoo.com>
2019-06-08 14:52:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,11 @@
-def create_an_empty_array = [Jason, Joe, Jared, Bonnie]
+def create_an_empty_array = []
 end
 
-def create_an_array
-  
+def create_an_array = [Jason, Joe, Jared, Bonnie]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(,)
+  create_an_array << 
 end
 
 def add_element_to_start_of_array(array, element)
